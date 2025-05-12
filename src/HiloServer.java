9ee408bff4576fbc4c2558bd631bb82d80bebb23
Mathías Terán <agusmaty23@gmail.com>
2025-05-12 15:21:39 -0500

    Mejorar la respuesta del servidor HiloServer para incluir el estado de gratuidad del estudiante en el mensaje de respuesta.

--- src/HiloServer.java
+++ src/HiloServer.java
@@ -15,23 +15,30 @@ public class HiloServer extends Thread {
         try {
             String received = new String(request.getData(), 0, request.getLength());
             int id = Integer.parseInt(received.trim());
+            String stringGratuidad = "";
 
             Estudiante estudiante = server.buscarEstudiantePorId(id);
             String responseMessage;
 
             if (estudiante != null) {
+                if (estudiante.isGratuidad()) {
+                    stringGratuidad = "SI";
+                } else {
+                    stringGratuidad = "NO";
+                }
                 responseMessage = "ID: " + estudiante.getId() + "\n" +
-                                  "Nombre: " + estudiante.getNombre() + "\n" +
-                                  "Teléfono: " + estudiante.getTelefono() + "\n" +
-                                  "Carrera: " + estudiante.getCarrera() + "\n" +
-                                  "Semestre: " + estudiante.getSemestre() + "\n" +
-                                  "Gratuidad: " + estudiante.isGratuidad();
+                        "Nombre: " + estudiante.getNombre() + "\n" +
+                        "Teléfono: " + estudiante.getTelefono() + "\n" +
+                        "Carrera: " + estudiante.getCarrera() + "\n" +
+                        "Semestre: " + estudiante.getSemestre() + "\n" +
+                        "Gratuidad: " + stringGratuidad + "\n";
             } else {
                 responseMessage = "Estudiante no encontrado";
             }
 
             byte[] responseBuffer = responseMessage.getBytes();
-            DatagramPacket response = new DatagramPacket(responseBuffer, responseBuffer.length, request.getAddress(), request.getPort());
+            DatagramPacket response = new DatagramPacket(responseBuffer, responseBuffer.length, request.getAddress(),
+                    request.getPort());
             socket.send(response);
         } catch (Exception e) {
             e.printStackTrace();
